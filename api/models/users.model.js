dbc8bed0929844942e78e267a491d81dac3c4662
Mohit Sharma <130201481+Mohitsholey04@users.noreply.github.com>
2023-10-20 16:03:31 +0530

    create user model

--- api/models/users.model.js
+++ api/models/users.model.js
@@ -3,20 +3,20 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        requires:true,
+        required:true,
         unique: true,
     },
     email: {
         type: String,
-        requires:true,
+        required:true,
         unique: true,
     },
     password: {
         type: String,
-        requires:true,
+        required:true,
     },
 }, {timestamps: true});
 
-const USer = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
